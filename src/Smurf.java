8c9f8794b0faf64a5277fe9bcd9491ae3eccdad1
kamronraiszadeh <league@WTS-IM-17.attlocal.net>
2018-09-22 12:55:53 -0700

    surf, backpack ip

--- src/Smurf.java
+++ src/Smurf.java
@@ -30,13 +30,27 @@ public class Smurf {
 	/* Papa Smurf wears a red hat, all the others are white. */
 	public String getHatColor() {
 		// 3. Fill in this method
-		return "";
+		if (name.equals("papa smurf")) {
+			return "hat is red";
+		}
+		else {
+			return "hat is white";
+		}
+	
 	}
 
 	/* Smurfette is the only female Smurf. */
 	public String isGirlOrBoy() {
 		// 4. Fill in this method
-		return "";
+		if (name.equals("smurfette")) {
+			return "female";
+		}
+		
+		else {
+			return "";
+		}
+		
+		
 	}
 
 }
--- src/smurfsrunner.java
+++ src/smurfsrunner.java
@@ -5,5 +5,28 @@ public static void main(String[] args) {
 	handy.eat();
 	handy.getName();
 	System.out.println(handy.getName());
+	Smurf papa=new Smurf ("papa smurf");
+
+	papa.getHatColor();
+	System.out.println(papa.getHatColor());
+	
+	papa.getName();
+	System.out.println(papa.getName());
+	papa.isGirlOrBoy();
+	
+	System.out.println(papa.isGirlOrBoy());
+	
+	Smurf smurfette=new Smurf ("smurfette");
+	
+	smurfette.getName();
+	System.out.println(smurfette.getName());
+	
+	smurfette.getHatColor();
+	System.out.println(smurfette.getHatColor());
+	
+	smurfette.isGirlOrBoy();
+	
+	System.out.println(smurfette.isGirlOrBoy());
+	
 }
 }
