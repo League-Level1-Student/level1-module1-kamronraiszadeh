48693f79d9280c8a1e0bf115587566ebdbc2b5c4
kamronraiszadeh <league@WorthyMini-6.attlocal.net>
2018-09-29 12:57:41 -0700

    backpack,taco

---
+++ src/taco.java
@@ -0,0 +1,31 @@
+
+public class taco {
+private String meat="";
+private String sauce="";
+
+public String getMeat() {
+	return meat;
+	
+	
+	
+}
+
+public void setMeat(String meat) {
+	
+	this.meat=meat;
+}
+
+public String getSauce() {
+	return sauce;
+	
+	
+	
+}
+public void setSauce(String sauce) {
+	
+	this.sauce=sauce;
+}
+
+
+
+}
